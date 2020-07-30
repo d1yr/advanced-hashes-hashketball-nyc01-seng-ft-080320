f8bc6e14303d7d045f1b84872a6339da009eb879
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-30 01:24:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,11 +204,16 @@ def player_stats(input)
     end
   end
   
-  def big_shoe_rebounds
+def big_shoe_rebounds
     big_shoe = 0 
     rebounds = 0 
     game_hash.each do |team, team_info|
-      team_info.each do |key, value|
-        
-        
+      team_info[:players].each do |player|
+        if player[:shoe] > big_shoe
+          big_shoe = player[:shoe]
+          rebounds = player[:rebounds]
+        end
+      end
+    end
+  return rebounds
 end
